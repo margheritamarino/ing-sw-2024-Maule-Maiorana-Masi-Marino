1b9a2681e0be6bbea0b54470ff7f6643dce31441
martinamaiorana <martina.maiorana@mail.polimi.it>
2024-03-24 16:25:57 +0100

    ResourceFront and ResourceBack modified

--- CodexNaturalis/src/main/java/it/polimi/ingsw/GoldBack.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/GoldBack.java
@@ -1,11 +1,22 @@
 package it.polimi.ingsw;
 
+import java.util.Random;
+
 public class GoldBack  extends GoldCard{
-    private ResourceType Resource;
+    final ResourceType Resource;
 
     // Costruttore
-    public GoldBack (ResourceType Resource) {
-        this.Resource = Resource;
+    public GoldBack (int cardID) {
+         { //Costruttore che assegna un valore RANDOMICO tra i 4 possibili all'attributo ResourceType
+            super.setCardID(cardID); // Chiama il costruttore della classe genitore per inizializzare l'ID della carta
+
+            // Ottieni un indice casuale per selezionare un valore casuale dall'enumerazione ResourceType
+            Random random = new Random(); //crea un nuovo oggetto della classe Random
+            int index = random.nextInt(ResourceType.values().length); // metodo nextInt(int n) della classe Random--> genera un numero intero casuale compreso tra 0 (incluso) e n (escluso)
+
+            // Assegna il valore corrispondente all'indice generato casualmente
+            this.Resource = ResourceType.values()[index];
+        }
     }
 
     // Metodo per ottenere il tipo di risorsa presente sulla carta
