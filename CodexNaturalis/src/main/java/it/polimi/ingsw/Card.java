206f5a8d6467408cf050f3b21b2469c3e0439a6d
Margherita <margherita.marino@mail.polimi.it>
2024-03-25 17:19:46 +0100

    Classes Corner and Card modified

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Card.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Card.java
@@ -1,14 +1,11 @@
 package it.polimi.ingsw;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
+import java.util.*;
 
 public abstract class Card {
     protected int cardID;
-    private Corner corner;
     private Map<Corner,Card> cornersMap;
     private  int numCorners;
-
+    //private Corner corner;
     private ResourceType mainResource;
     private int numResources;
     private int numSymbols;
@@ -18,47 +15,13 @@ public abstract class Card {
     //costruttore
     public Card(int cardId){
         this.cardID = cardID;
+        this.mainResource = setRandomMainResource();
         this.cornersMap = new HashMap<>();
     }
     public int getCardID() {
         return cardID;
     }
 
-    public int getNumResources() {
-        return numResources;
-    }
-
-    public int getNumSymbols() {
-        return numSymbols;
-    }
-
-    public void setNumResources(int numResources) {
-        this.numResources = numResources;
-    }
-
-    public void setNumSymbols(int numSymbols) {
-        this.numSymbols = numSymbols;
-    }
-
-    public ResourceType getMainResource() {
-        return mainResource;
-    }
-
-    public void setMainResource(ResourceType mainResource) {
-        this.mainResource = mainResource;
-    }
-
-    public int getNumCorners() {
-        return numCorners;
-    }
-
-    public void setNumCorners(int numCorners) {
-        this.numCorners = numCorners;
-    }
-
-    public Map<Corner, Card> getCornersMap() {
-        return cornersMap;
-    }
 
     public void setCornersMap(Map<Corner, Card> cornersMap) {
         this.cornersMap = cornersMap;
@@ -75,5 +38,35 @@ public abstract class Card {
     }
     public Card getConnectedCard(Corner corner) {
         return cornersMap.get(corner);
+    }
+
+
+    /**
+     * @author Margherita Marino, Martina Maiorana
+     * given the number of corners, this method creates the required number of corners, with a randomic CornerType
+     * @param numCorners number of corners in card
+     */
+
+    private void setRandomCornersMap(int numCorners) {
+        List<CornerType> cornerTypes = new ArrayList<>();
+        Collections.addAll(cornerTypes, CornerType.values());
+        Collections.shuffle(cornerTypes);
+        int i;
+        for (i = 1; i <= numCorners; i++) {
+            this.cornersMap.put(new Corner(cornerTypes.get(i), null, null), null);
+        }
+    }
+
+
+
+    private ResourceType setRandomMainResource() {
+        List<ResourceType> resourceTypes = new ArrayList<>();
+        Collections.addAll(resourceTypes, ResourceType.values());
+        Collections.shuffle(resourceTypes);
+        return resourceTypes.get(0);
+    }
+
+    public ResourceType getMainResource() {
+        return mainResource;
     }
 }
