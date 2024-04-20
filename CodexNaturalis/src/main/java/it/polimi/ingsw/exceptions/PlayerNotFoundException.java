a97e5d223ef78276edd48ddff72018ee485e026e
masiirene <irenepia.masi@mail.polimi.it>
2024-04-20 19:57:54 +0200

    New exception added

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/exceptions/PlayerNotFoundException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.exceptions;
+
+public class PlayerNotFoundException extends Exception {
+    public PlayerNotFoundException(String message) {
+        super(message);
+    }
+}
