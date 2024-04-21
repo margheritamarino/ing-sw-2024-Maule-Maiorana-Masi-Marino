4d1b163a52b70013a8b681fa55b282a2de9cce7f
Sofia Maule <sofia.maule@mail.polimi.it>
2024-04-21 09:30:30 +0200

    21/04/2024  game exceptions

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/exceptions/InvalidPointsException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.exceptions;
+
+public class InvalidPointsException extends Exception{
+    public InvalidPointsException(String message) {
+        super(message);
+    }
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/exceptions/NoPlayersException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.exceptions;
+
+public class NoPlayersException extends Exception{
+    public NoPlayersException(String message) {
+        super(message);
+    }
+}
