a4469530fbf14606618c8ed28f63e0b2a13401e9
masiirene <irenepia.masi@mail.polimi.it>
2024-04-21 16:44:38 +0200

    21/04/2024

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Listener/GameListener.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.Listener;
+
+public interface GameListener {
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Listener/ListenersHandler.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.Listener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * ListenersHandler class is responsible for managing a List of GameListener objects
+ * and for notifying the view when a change occurs in the GameModel
+ */
+public class ListenersHandler {
+    private List<GameListener> listeners;
+
+    /**
+     * Constructor
+     */
+    public ListenersHandler() {
+        listeners = new ArrayList<>();
+    }
+
+
+
+}
