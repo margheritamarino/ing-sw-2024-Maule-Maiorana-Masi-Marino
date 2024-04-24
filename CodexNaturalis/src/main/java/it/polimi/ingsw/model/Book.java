6d20601c3e69082d0918452f8ccc3a34b14e9ac2
Sofia Maule <sofia.maule@mail.polimi.it>
2024-04-24 10:55:55 +0200

    initializeDeck

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Book.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Book.java
@@ -1,4 +1,8 @@
 package it.polimi.ingsw.model;
+import it.polimi.ingsw.model.SymbolType;
+import it.polimi.ingsw.model.ResourceType;
+import it.polimi.ingsw.model.Cell;
+
 
 import it.polimi.ingsw.exceptions.CellNotAvailableException;
 import it.polimi.ingsw.model.cards.ObjectiveCard;
