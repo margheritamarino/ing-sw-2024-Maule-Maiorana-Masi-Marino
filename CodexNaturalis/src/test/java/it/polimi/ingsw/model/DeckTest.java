10b457f662ca517c3203eb7072b3497f179f78dc
Sofia Maule <sofia.maule@mail.polimi.it>
2024-04-25 13:44:20 +0200

    Deck exception

--- CodexNaturalis/src/test/java/it/polimi/ingsw/model/DeckTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/model/DeckTest.java
@@ -4,12 +4,12 @@ import it.polimi.ingsw.exceptions.DeckEmptyException;
 import it.polimi.ingsw.exceptions.FileReadException;
 import it.polimi.ingsw.model.cards.CardType;
 import it.polimi.ingsw.model.cards.PlayableCard;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 
-import static junit.framework.TestCase.*;
 import static org.junit.Assert.assertThrows;
 
 class DeckTest {
@@ -28,15 +28,15 @@ class DeckTest {
     @Test
     void testInitializeDeck() {
         // Verifica che il mazzo sia inizializzato correttamente
-        assertEquals(40, deckGold.getNumCards());
-        assertEquals(40, deckResource.getNumCards());
-        assertEquals(6, deckInitial.getNumCards());
-        assertNotNull(deckGold.getFrontCards());
-        assertNotNull(deckGold.getBackCards());
-        assertNotNull(deckResource.getFrontCards());
-        assertNotNull(deckResource.getBackCards());
-        assertNotNull(deckInitial.getFrontCards());
-        assertNotNull(deckInitial.getBackCards());
+        Assertions.assertEquals(40, deckGold.getNumCards());
+        Assertions.assertEquals(40, deckResource.getNumCards());
+        Assertions.assertEquals(6, deckInitial.getNumCards());
+        Assertions.assertNotNull(deckGold.getFrontCards());
+        Assertions.assertNotNull(deckGold.getBackCards());
+        Assertions.assertNotNull(deckResource.getFrontCards());
+        Assertions.assertNotNull(deckResource.getBackCards());
+        Assertions.assertNotNull(deckInitial.getFrontCards());
+        Assertions.assertNotNull(deckInitial.getBackCards());
     }
 
     @Test
@@ -46,12 +46,12 @@ class DeckTest {
         PlayableCard[] cardsResource = deckResource.returnCard();
         PlayableCard[] cardsInitial = deckInitial.returnCard();
 
-        assertNotNull(cardsGold);
-        assertNotNull(cardsResource);
-        assertNotNull(cardsInitial);
-        assertEquals(39, deckGold.getNumCards());
-        assertEquals(39, deckResource.getNumCards());
-        assertEquals(5, deckInitial.getNumCards());
+        Assertions.assertNotNull(cardsGold);
+        Assertions.assertNotNull(cardsResource);
+        Assertions.assertNotNull(cardsInitial);
+        Assertions.assertEquals(39, deckGold.getNumCards());
+        Assertions.assertEquals(39, deckResource.getNumCards());
+        Assertions.assertEquals(5, deckInitial.getNumCards());
     }
 
     @Test
@@ -61,30 +61,28 @@ class DeckTest {
             try {
                 deckGold.returnCard();
             } catch (DeckEmptyException e) {
-                fail("DeckEmptyException should not be thrown before the deck is empty.");
+                Assertions.fail("DeckEmptyException should not be thrown before the deck is empty.");
             }
         }
 
-        assertThrows(DeckEmptyException.class, () -> {
-            deckGold.returnCard();
-        });
+        assertThrows(DeckEmptyException.class, () -> deckGold.returnCard());
     }
 
     @Test
     void testCheckEndDeck() {
         // Verifica che il mazzo finisca correttamente
-        assertFalse(deckGold.checkEndDeck());
-        assertFalse(deckResource.checkEndDeck());
-        assertFalse(deckInitial.checkEndDeck());
+        Assertions.assertFalse(deckGold.checkEndDeck());
+        Assertions.assertFalse(deckResource.checkEndDeck());
+        Assertions.assertFalse(deckInitial.checkEndDeck());
 
         // Estrai tutte le carte dal mazzo Gold
         while (!deckGold.checkEndDeck()) {
             try {
                 deckGold.returnCard();
             } catch (DeckEmptyException e) {
-                fail("DeckEmptyException should not be thrown before the deck is empty.");
+                Assertions.fail("DeckEmptyException should not be thrown before the deck is empty.");
             }
         }
-        assertTrue(deckGold.checkEndDeck());
+        Assertions.assertTrue(deckGold.checkEndDeck());
     }
 }
