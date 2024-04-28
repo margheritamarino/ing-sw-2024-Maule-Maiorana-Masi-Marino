3b51edd58f45ab1282b6f395ff3a3f6506974ade
Sofia Maule <sofia.maule@mail.polimi.it>
2024-04-28 15:41:15 +0200

    RMI

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/ClientSkeleton.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.network;
-
-/**
- * The client skeleton class represents a client's connection to the server. It implements the Client interface and
- * provides methods for sending and receiving messages to/from the server.
- */
-public class ClientSkeleton implements Client{
-
-}
--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/ServerStub.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.network;
-/**
- * The ServerStub class represents a client-side stub for the server.
- * It is responsible for establishing a connection to the server, sending and receiving messages.
- */
-public class ServerStub implements Server{
-}
