b831f67cfd4fc306f6a69c8ee2a13c7f479fe15e
masiirene <irenepia.masi@mail.polimi.it>
2024-04-30 16:56:44 +0200

    Change

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/flow/Flow.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/flow/Flow.java
@@ -3,32 +3,24 @@ package it.polimi.ingsw.view.flow;
 import it.polimi.ingsw.listener.GameListener;
 import it.polimi.ingsw.model.game.GameImmutable;
 
+
+//classe astratta che fornisce funzionalità di base comuni che possono essere estese dal GameFlow
 public abstract class Flow implements GameListener {
 
-    /**
-     * Resets the game id
-     * @param fileDisconnection file to reset
-     * @param model
-     */
+
+    //reimposta l'ID del gioco per tutti i players quando il gioco viene resettato o terminato
     protected void resetGameId(FileDisconnection fileDisconnection, GameImmutable model) {
         for (PlayerIC p : model.getPlayers()) {
             fileDisconnection.setLastGameId(p.getNickname(), -1);
         }
     }
 
-    /**
-     * Saves latest game id
-     * @param fileDisconnection file to write
-     * @param nick
-     * @param gameId
-     */
+    //salvo l'ultimo ID del gioco associato al giocatore nel file di disconnessione
     protected void saveGameId(FileDisconnection fileDisconnection, String nick, int gameId) {
         fileDisconnection.setLastGameId(nick, gameId);
     }
 
-    /**
-     * Shows no connection error
-     */
+    //gestisce gli errori di mancata connessione e deve essere implementato specificatamente per l'interfaccia utente che si utilizza (TUI o GUI)
     public abstract void noConnectionError();
 
 }
