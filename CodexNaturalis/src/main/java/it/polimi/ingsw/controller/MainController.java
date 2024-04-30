c6e4f13cc5fb3b3e3935256d2b4e2ea162532a97
Sofia Maule <sofia.maule@mail.polimi.it>
2024-04-30 12:33:08 +0200

    RMI iniziale

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/MainController.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.listener.GameListener;
+import it.polimi.ingsw.network.rmi.GameControllerInterface;
+import it.polimi.ingsw.network.rmi.MainControllerInterface;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+
+public class MainController implements MainControllerInterface, Serializable {
+    @Override
+    public GameControllerInterface createGame(GameListener lis, String nick) throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public GameControllerInterface joinFirstAvailableGame(GameListener lis, String nick) throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public GameControllerInterface joinGame(GameListener lis, String nick, int idGame) throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public GameControllerInterface reconnect(GameListener lis, String nick, int idGame) throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public GameControllerInterface leaveGame(GameListener lis, String nick, int idGame) throws RemoteException {
+        return null;
+    }
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/PrintAsync.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.view;
+
+public class PrintAsync {
+    /**
+     * Avoid freezing console for quick edit mode in console
+     * NB If the server prints strings and you select the text, the quick edit mode pauses the system.out until
+     * a key is pressed.
+     * Doing so, not only the prints are paused but also the main thread is prevented from being blocked.
+     * With this method quick edit mode pause only the prints avoiding the pausing of the main thread running the code
+     *
+     * @param msg to print
+     */
+    /*
+    l'utilizzo di un nuovo thread per la stampa consente al programma principale di continuare a eseguire
+    il suo lavoro senza essere bloccato dall'output sulla console. Questo è particolarmente
+     utile in situazioni in cui la console potrebbe essere bloccata, ad esempio a causa della modalità di modifica rapida.     */
+   /* public static void printAsync(Ansi msg){
+        new Thread(()->{System.out.println(msg);}).start();
+    }*/
+    public static void printAsync(String msg){
+        new Thread(()->{System.out.println(msg);}).start();
+    }
+    public static void printAsync(StringBuilder msg){
+        new Thread(()->{System.out.println(msg);}).start();
+    }
+    public static void printAsyncNoLine(String msg){
+        new Thread(()->{System.out.print(msg);}).start();
+    }
+}
