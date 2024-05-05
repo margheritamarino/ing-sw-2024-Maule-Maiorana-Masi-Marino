874b041a75753fda407326a83982b777ba54f684
martinamaiorana <martina.maiorana@mail.polimi.it>
2024-05-05 17:17:00 +0200

    Corrections

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/Main/ClientMain.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/Main/ClientMain.java
@@ -1,7 +1,134 @@
 package it.polimi.ingsw.network.Main;
 
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
 public class ClientMain {
+
     public static void main(String[] args) {
+        clearCMD();
+        int selection;
+
+        //Disable javaFX logger
+        killLoggers();
+
+        if (!DefaultValue.DEBUG) {
+            String input;
+
+            do {
+                printAsync(ansi().cursor(1, 0).a("""
+                        Insert remote IP (leave empty for localhost)
+                        """));
+                input = new Scanner(System.in).nextLine();
+                if(!input.equals("") && !isValidIP(input)){
+                    clearCMD();
+                    printAsync("Not valid");
+                }
+            } while (!input.equals("") && !isValidIP(input));
+            if (!input.equals(""))
+                DefaultValue.serverIp = input;
+
+            clearCMD();
+
+            do {
+                printAsync(ansi().cursor(1, 0).a("""
+                        Insert your IP (leave empty for localhost)
+                        """));
+                input = new Scanner(System.in).nextLine();
+                if(!input.equals("") && !isValidIP(input)){
+                    clearCMD();
+                    printAsync("Not valid");
+                }
+            } while (!input.equals("") && !isValidIP(input));
+            if (!input.equals(""))
+                System.setProperty("java.rmi.server.hostname", input);
+
+
+            clearCMD();
+            do {
+                printAsync(ansi().cursor(1, 0).a("""
+                        Select option:
+                        \t (1) TUI + Socket
+                        \t (2) TUI + RMI
+                        \t
+                        \t (3) GUI + Socket
+                        \t (4) GUI + RMI
+                        """));
+                input = new Scanner(System.in).nextLine();
+                try {
+                    selection = Integer.parseInt(input);
+                } catch (NumberFormatException e) {
+                    selection = -1;
+                    printAsync("Nan");
+                }
+            } while (selection != 1 && selection != 2 && selection != 3 && selection != 4);
+        } else {
+            selection = 2; //Default run configuration
+        }
+
+
+        //Get the Communication Protocol wanted
+        ConnectionSelection conSel;
+        if (selection == 1 || selection == 3) {
+            conSel = ConnectionSelection.SOCKET;
+        } else {
+            conSel = ConnectionSelection.RMI;
+        }
+
+        printAsync("Starting the game!");
+
+        //Starts the UI wanted
+        if (selection == 1 || selection == 2) {
+            //Starts the game with TUI
+            //I can start directly here the GameFlow
+            new GameFlow(conSel);
+        } else {
+            //Starts the game with GUI
+            //For doing so, I need to start the Main of GUI (GameFlow needs to be started inside the thread of GUI)
+            Application.launch(GUIApplication.class, conSel.toString());
+        }
 
     }
+
+    private static void clearCMD() {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (IOException | InterruptedException e) {
+            printAsync("\033\143");   //for Mac
+        }
+    }
+
+    private static boolean isValidIP(String input) {
+        List<String> parsed;
+        parsed = Arrays.stream(input.split("\\.")).toList();
+        if (parsed.size() != 4) {
+            return false;
+        }
+        for (String part : parsed) {
+            try {
+                Integer.parseInt(part);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void killLoggers(){
+        com.sun.javafx.util.Logging.getJavaFXLogger().disableLogging();
+        com.sun.javafx.util.Logging.getCSSLogger().disableLogging();
+        com.sun.javafx.util.Logging.getAccessibilityLogger().disableLogging();
+        com.sun.javafx.util.Logging.getFocusLogger().disableLogging();
+        com.sun.javafx.util.Logging.getInputLogger().disableLogging();
+        com.sun.javafx.util.Logging.getLayoutLogger().disableLogging();
+    }
+
 }
+
+
+
