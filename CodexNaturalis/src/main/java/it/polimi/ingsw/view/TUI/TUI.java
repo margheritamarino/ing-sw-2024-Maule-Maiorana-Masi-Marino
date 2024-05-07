246fcad889a1efb26be5a0207f3cb157fa6cc32e
masiirene <irenepia.masi@mail.polimi.it>
2024-05-07 11:27:47 +0200

    TUI

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/TUI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/TUI.java
@@ -130,6 +130,90 @@ public class TUI extends UI {
         printAsync(model.getCurrentPlayer().getPlayerDeckIC().toString());
     }
 
+    @Override
+    public void show_notValidMessage(){
+        printAsync("Not Valid input");
+    }
+
+    @Override
+    public void show_playerJoined(GameImmutable gameModel, String nick) {
+        clearScreen();
+        show_welcome(nick);
+        printAsync(ansi().fg(YELLOW).a("GameID: [" + gameModel.getGameId().toString() + "]\n"));
+        System.out.flush(); //Svuota il buffer di output per garantire che tutti i dati scritti siano visibili immediatamente
+    }
+
+    @Override
+    public void show_readyToStart(GameImmutable gameModel, String nickname) {
+        printAsync("Press y if you are ready to start the game");
+    }
+
+    @Override
+    public void show_youAreReady(GameImmutable model){
+        printAsync("You are ready to start the game!");
+    }
+
+    @Override
+    public void show_whichInitialCards() {
+        printAsync("> Choose the front or the back of  this initial card:");
+    }
+
+    @Override
+    public void show_whichObjectiveCards() {
+        printAsync("> Choose one of this two objective cards:");
+    }
+
+    /**
+     * Clears the console
+     */
+    public void clearScreen(){
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            //if not on a Windows machine
+        } catch (IOException | InterruptedException e) {
+            //for mac
+            printAsyncNoCursorReset("\033\143");
+        }
+    }
+
+    /**
+     * Shows the game id
+     * @param gameModel
+     */
+    public void show_gameId(GameImmutable gameModel) {
+        printAsync(ansi().fg(DEFAULT).a("Game with id: [" + gameModel.getGameId() + "]"));
+    }
+
+    @Override
+    public void show_gameStarted(GameImmutable model) throws IOException, InterruptedException {
+        this.clearScreen();
+        this.show_publisher();
+        this.show_titleCodexNaturalis();
+        this.show_allPlayers(model); //mostra la lista dei giocatori
+        this.show_alwaysShowForAll(model);
+        this.show_gameId(model);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /**
      * @param input the string of the important event to add
      */
@@ -181,29 +265,7 @@ public class TUI extends UI {
     }
 
 
-    @Override
-    public void show_whichInitialCards() {
-        printAsync("> Choose the front or the back of  this initial card:");
-    }
 
-    @Override
-    public void show_whichObjectiveCards() {
-        printAsync("> Choose one of this two objective cards:");
-    }
-
-
-    /**
-     * Clears the console
-     */
-    public void clearScreen(){
-        try {
-            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            //if not on a Windows machine
-        } catch (IOException | InterruptedException e) {
-            //for mac
-            printAsyncNoCursorReset("\033\143");
-        }
-    }
 
 
     /**
@@ -246,23 +308,6 @@ public class TUI extends UI {
     }
 
 
-    /**
-     * Shows the game id
-     * @param gameModel
-     */
-    public void show_gameId(GameImmutable gameModel) {
-        //printAsync(ansi().cursor(DefaultValue.row_gameID, 0).bold().a("Game with id: [" + gameModel.getGameId() + "]").boldOff());
-    }
-
-    @Override
-    public void show_gameStarted(GameImmutable model) throws IOException, InterruptedException {
-        this.clearScreen();
-        this.show_publisher();
-        this.show_titleCodexNaturalis();
-        this.show_allPlayers(model); //mostra la lista dei giocatori
-        this.show_alwaysShowForAll(model);
-        this.show_gameId(model);
-    }
 
 
     public void show_card(GameImmutable model, int index){
@@ -284,28 +329,6 @@ public class TUI extends UI {
     }
     public void show_ChooseObjectiveCard(){
 
-    }
-    @Override
-    public void show_playerJoined(GameImmutable gameModel, String nick) {
-        clearScreen();
-        show_welcome(nick);
-        printAsync(ansi().fg(YELLOW).a("GameID: [" + gameModel.getGameId().toString() + "]\n"));
-        System.out.flush(); //Svuota il buffer di output per garantire che tutti i dati scritti siano visibili immediatamente
-    }
-
-    @Override
-    public void show_readyToStart(GameImmutable gameModel, String nickname) {
-        new PrintStream(System.out, true, System.console() != null
-                ? System.console().charset()
-                : Charset.defaultCharset() //se non è disponibile la console viene usato il set di caratteri predefinito
-        ).println(ansi().fg(DEFAULT).a("PRESS y if you are ready to start the game").reset());
-    }
-
-    public void show_youAreReady(GameImmutable model){
-        new PrintStream(System.out, true, System.console() != null
-                ? System.console().charset()
-                : Charset.defaultCharset() //se non è disponibile la console viene usato il set di caratteri predefinito
-        ).println(ansi().fg(DEFAULT).a("You are ready to start the game!").reset());
     }
 
     @Override
