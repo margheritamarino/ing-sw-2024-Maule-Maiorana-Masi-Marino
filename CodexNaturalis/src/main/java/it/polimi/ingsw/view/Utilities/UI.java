cbba898126e26ec1af866a17f02202b7c0ffc220
Margherita Marino <margherita.marino@mail.polimi.it>
2024-05-07 13:25:54 +0200

    toString methods

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/Utilities/UI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/Utilities/UI.java
@@ -21,7 +21,6 @@ public abstract class UI {
      * Initialize GUI or TUI
      */
     public abstract void init();
-
     /**
      * Show's Cranio Creation's logo
      *
@@ -29,7 +28,6 @@ public abstract class UI {
      * @throws InterruptedException
      */
     public abstract void show_publisher() throws IOException, InterruptedException;
-
     public abstract void show_askPlaceCardsMainMsg(GameImmutable model);
     public abstract void show_alwaysShow(GameImmutable model, String nickname);
     public abstract void show_alwaysShowForAll(GameImmutable model);
@@ -37,69 +35,31 @@ public abstract class UI {
     public abstract void show_cardPlacedMsg(GameImmutable model);
     public abstract void show_pointsAddedMsg(GameImmutable model);
     public abstract void show_joiningToGameMsg(String nick);
-
-
-    //show per mostrare come unirsi al gioco
-    public abstract void show_menuOptions();
-
-    public abstract void show_creatingNewGameMsg(String nickname);
-    /**
-     * Shows the join first available game message
-     * @param nickname player's nickname
-     */
-    protected abstract void show_joiningFirstAvailableMsg(String nickname);
-
-
     /**
      * Asks the player for his nickname
      */
     public abstract void show_insertNicknameMessage();
-
     /**
      * Shows the player's chosen nickname
      *
      * @param nickname nickname just chosen by the player
      */
     public abstract void show_chosenNickname(String nickname);
-
-    /**
-     * Shows error message when there are no games available for joining
-     *
-     * @param msg message that needs visualisation
-     */
-    protected abstract void show_noAvailableGamesToJoin(String msg);
-
-    /**
-     * Asks the player for number of players for a new game
-     */
-    protected abstract void show_insertPlayersNumber();
-
     /**
      * Shows game started message
      * @param model model where the game has started
      */
-    public abstract void show_gameStarted(GameImmutable model) throws IOException, InterruptedException;
+    public abstract void show_gameStarted(GameImmutable model);
     /**
      * Shows the playerDeck
      * @param model
      */
-    protected abstract void show_playerDeck(GameImmutable model);
-    /**
-     * Shows the game ended message
-     * @param model where the game is ended
-     */
     public abstract void show_gameEnded(GameImmutable model);
-
-
     public abstract void show_notValidMessage();
-
-    public abstract void show_temporaryInitialCards(GameImmutable model) throws FileNotFoundException, FileReadException;
-    protected abstract void show_ObjectiveCards(GameImmutable model);
-
     public abstract void show_playerJoined(GameImmutable gameModel, String nick);
     public abstract void show_allPlayers(GameImmutable model);
     public abstract void show_youAreReady(GameImmutable model);
-
+    protected abstract void show_playerDeck(GameImmutable model);
     /**
      * Shows that the playing player is ready to start
      *
@@ -107,7 +67,46 @@ public abstract class UI {
      * @param nickname player's nickname
      */
     public abstract void show_readyToStart(GameImmutable gameModel, String nickname);
+    public abstract void show_returnToMenuMsg();
+    public abstract void show_whichInitialCards();
+    public abstract void show_whichObjectiveCards();
+    /**
+     * Shows message on important event added
+     * @param input the string of the important event to add
+     */
+    public abstract void addImportantEvent(String input);
+    /**
+     * Resets the important events
+     */
+    public abstract void resetImportantEvents();
 
+
+
+
+
+
+
+
+
+
+
+    //TODO
+    public abstract void show_wrongSelectionInitialMsg();
+    public abstract void show_wrongSelectionObjectiveMsg();
+    protected abstract void show_askNum(String msg, GameImmutable gameModel, String nickname);
+    public abstract void show_temporaryInitialCards(GameImmutable model) throws FileNotFoundException, FileReadException;
+    protected abstract void show_ObjectiveCards(GameImmutable model);
+
+
+
+
+
+
+
+    /**
+     * Shows an error when there's no connection
+     */
+    protected abstract void show_noConnectionError();
     /**
      * Show the message for next turn or reconnected player
      *
@@ -115,70 +114,6 @@ public abstract class UI {
      * @param nickname nick of reconnected player (or of the player that is now in turn)
      */
     protected abstract void show_nextTurnOrPlayerReconnected(GameImmutable model, String nickname);
-
-
-    //SHOW DEI METODI ASK
-
-    public abstract void show_returnToMenuMsg();
-
-    protected abstract void show_askNum(String msg, GameImmutable gameModel, String nickname);
-    public abstract void show_whichInitialCards();
-
-    public abstract void show_whichObjectiveCards();
-
-    public void show_insertGameIDMessage() {
-    }
-
-    protected abstract void show_pickCellRequest();
-
-    /**
-     * Show the message for pick card
-     */
-    protected abstract void show_pickCardRequest();
-
-    /**
-     * Show the message for pick deck
-     */
-    protected abstract void show_pickDeckRequest();
-
-    /**
-     * Show the message for choosing Array Position
-     */
-    protected abstract void show_chooseArrayPosition();
-
-    /**
-     * Show reset information game
-     */
-    protected abstract void show_InformationGame();
-
-//other
-    // es show_addedPoints
-
-
-
-    //METODI CON AZIONI
-    /**
-     * Shows message on important event added
-     * @param input the string of the important event to add
-     */
-    public abstract void addImportantEvent(String input);
-
-    /**
-     * Resets the important events
-     */
-    public abstract void resetImportantEvents();
-
-    /**
-     * Shows an error when there's no connection
-     */
-    protected abstract void show_noConnectionError();
-
-
-    public abstract void show_wrongSelectionInitialMsg();
-
-    public abstract void show_wrongSelectionObjectiveMsg();
-
-
 
 
 }
