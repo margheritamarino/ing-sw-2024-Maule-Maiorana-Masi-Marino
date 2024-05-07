0c7d6107f06ad30e388646f21f1abc7341366453
Sofia Maule <sofia.maule@mail.polimi.it>
2024-05-07 16:37:03 +0200

    MsgPickCard

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/Messages/clientToServerMessages/ClientMsgPickCard.java
@@ -0,0 +1,39 @@
+package it.polimi.ingsw.network.socket.Messages.clientToServerMessages;
+
+import it.polimi.ingsw.controller.GameController;
+import it.polimi.ingsw.exceptions.NotPlayerTurnException;
+import it.polimi.ingsw.listener.GameListenerInterface;
+import it.polimi.ingsw.model.cards.CardType;
+import it.polimi.ingsw.network.rmi.GameControllerInterface;
+
+import java.rmi.RemoteException;
+
+public class ClientMsgPickCard extends ClientGenericMessage {
+    CardType cardType;
+    boolean drawFromDeck;
+    int pos;
+
+
+    public ClientMsgPickCard(String nickname, CardType cardType, boolean drawFromDeck, int pos) {
+        this.nickname = nickname;
+        this.cardType = cardType ;
+        this.drawFromDeck = drawFromDeck;
+        this.pos = pos;
+    }
+
+
+    @Override
+    public GameControllerInterface execute(GameListenerInterface lis, GameController gameController) throws RemoteException {
+        return null;
+    }
+
+    /**
+     * Method to execute the corresponding action for the message.
+     * @param gameController the game controller interface
+     * @throws RemoteException if there is an error in remote communication
+     */
+    @Override
+    public void execute(GameControllerInterface gameController) throws RemoteException {
+        gameController.PickCardFromBoard(nickname, cardType, drawFromDeck, pos);
+    }
+}
