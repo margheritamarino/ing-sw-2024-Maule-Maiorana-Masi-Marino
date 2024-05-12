872a4871b3851ba5692cb5a23ff9bd8f7db92620
martinamaiorana <martina.maiorana@mail.polimi.it>
2024-05-12 18:27:22 +0200

    Uploading Socket Network

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/Messages/clientToServerMessages/ClientMsgPickCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/Messages/clientToServerMessages/ClientMsgPickCard.java
@@ -23,8 +23,8 @@ public class ClientMsgPickCard extends ClientGenericMessage {
 
 
     @Override
-    public GameControllerInterface execute(GameListenerInterface lis, GameController gameController) throws RemoteException {
-        return null;
+    public void execute(GameListenerInterface lis, GameController gameController) throws RemoteException {
+
     }
 
     /**
