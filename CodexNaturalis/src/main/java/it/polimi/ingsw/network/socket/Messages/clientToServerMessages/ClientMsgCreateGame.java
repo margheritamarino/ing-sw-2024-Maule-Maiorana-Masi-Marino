241defc9c41f168fbb16d07a195e7a38b298ecf6
Sofia Maule <sofia.maule@mail.polimi.it>
2024-05-15 14:39:28 +0200

    createGame

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/Messages/clientToServerMessages/ClientMsgCreateGame.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.network.socket.Messages.clientToServerMessages;
+
+import it.polimi.ingsw.controller.GameController;
+import it.polimi.ingsw.listener.GameListenerInterface;
+import it.polimi.ingsw.network.rmi.GameControllerInterface;
+
+import java.rmi.RemoteException;
+
+public class ClientMsgCreateGame extends ClientGenericMessage {
+    int numPlayers;
+    int GameID;
+    /**
+     * Constructor of the class.
+     * @param nickname the player's nickname
+     */
+    public ClientMsgCreateGame(int numPlayers, int GameID, String nickname) {
+        this.numPlayers= numPlayers;
+        this.GameID=GameID;
+        this.nickname = nickname;
+
+        this.isJoinGame= true;
+    }
+
+    @Override
+    public void execute(GameListenerInterface lis, GameController gameController) throws RemoteException {
+        gameController.createGame(lis,numPlayers, GameID, this.nickname);
+    }
+
+
+    @Override
+    public void execute(GameControllerInterface gameController) throws RemoteException {
+
+    }
+
+
+}
+
+
