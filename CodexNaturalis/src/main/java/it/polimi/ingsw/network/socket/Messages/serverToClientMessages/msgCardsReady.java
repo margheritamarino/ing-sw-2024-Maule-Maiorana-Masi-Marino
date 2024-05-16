667e0385000d085156fa67140adbf079255ec2e9
Sofia Maule <sofia.maule@mail.polimi.it>
2024-05-16 14:59:57 +0200

    Messages

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/Messages/serverToClientMessages/msgCardsReady.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.network.socket.Messages.serverToClientMessages;
+
+import it.polimi.ingsw.listener.GameListenerInterface;
+import it.polimi.ingsw.model.game.GameImmutable;
+
+import java.rmi.RemoteException;
+
+public class msgCardsReady extends ServerGenericMessage {
+    private final GameImmutable model;
+    private final String nickname;
+
+    /**
+     * Constructor of the class.
+     * @param model the immutable game model
+     */
+    public msgCardsReady(GameImmutable model, String nickname) {
+        this.model = model;
+        this.nickname = nickname;
+    }
+
+    /**
+     * Method to execute the corresponding action for the message.
+     * @param lis the game listener
+     * @throws RemoteException if there is an error in remote communication
+     */
+    @Override
+    public void execute(GameListenerInterface lis) throws RemoteException {
+        lis.cardsReady(model, nickname);
+    }
+
+}
+
