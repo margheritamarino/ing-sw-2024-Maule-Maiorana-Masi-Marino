5df40d80f9a24c809aa600f82f2ea94ff9a8208f
martinamaiorana <martina.maiorana@mail.polimi.it>
2024-05-20 16:37:24 +0200

    corrected GuiApplication

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/GUIApplication.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/GUIApplication.java
@@ -131,11 +131,14 @@ public class GUIApplication extends Application {
                     openPopup(scenes.get(getSceneIndex(scene)).getScene());
                     return;
                 }
-                case MENU -> {
-                    this.primaryStage.centerOnScreen();
-                    this.primaryStage.setAlwaysOnTop(false);
-                    MenuController controller = (MenuController) s.getControllerGUI();
+                case ASK_NUM_PLAYERS -> {
+                    //TODO
                 }
+                case  ASK_GAME_ID-> {
+                    //TODO
+                }
+
+
                 default -> {
                     this.primaryStage.setAlwaysOnTop(false);
 
