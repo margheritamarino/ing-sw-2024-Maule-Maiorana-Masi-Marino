247d14d1aae4db100d0fa82e470bfcd2c6c4c24d
Margherita Marino <margherita.marino@mail.polimi.it>
2024-05-21 08:24:08 +0200

    GUI 2

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/controllers/GameIDController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/controllers/GameIDController.java
@@ -1,4 +1,23 @@
 package it.polimi.ingsw.view.GUI.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.io.IOException;
+
 public class GameIDController extends ControllerGUI{
+    @FXML
+    private TextField numGameId; //campo in cui l'utente inserisce il proprio nome
+
+    /**
+     * Method to control the nickname.
+     * @param e the action event
+     * @throws IOException if there are connection problems
+     */
+    public void actionEnter(ActionEvent e) throws IOException {
+        if (!numGameId.getText().isEmpty()) {
+            getInputGUI().addTxt(numGameId.getText());
+        }
+    }
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/controllers/NicknameController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/controllers/NicknameController.java
@@ -13,7 +13,7 @@ public class NicknameController extends ControllerGUI {
 
 
     @FXML
-    private TextField nickname; //campo in cui l'utente inserisce il proprio nome
+    private TextField nicknameTextField; //campo in cui l'utente inserisce il proprio nome
 
     /**
      * Method to control the nickname.
@@ -21,8 +21,8 @@ public class NicknameController extends ControllerGUI {
      * @throws IOException if there are connection problems
      */
     public void actionEnter(ActionEvent e) throws IOException {
-        if (!nickname.getText().isEmpty()) {
-            getInputGUI().addTxt(nickname.getText());
+        if (!nicknameTextField.getText().isEmpty()) {
+            getInputGUI().addTxt(nicknameTextField.getText());
         }
     }
 }
