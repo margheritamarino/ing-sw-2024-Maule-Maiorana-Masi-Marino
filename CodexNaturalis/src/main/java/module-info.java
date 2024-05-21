cbbce1fb5c35f9bfa30742028e1c6afbe8a31f8e
masiirene <irenepia.masi@mail.polimi.it>
2024-05-21 21:12:30 +0200

    module-info

--- CodexNaturalis/src/main/java/module-info.java
+++ CodexNaturalis/src/main/java/module-info.java
@@ -6,7 +6,10 @@ module PSP24 {
     requires org.fusesource.jansi;
     requires java.desktop;
     exports it.polimi.ingsw.view.GUI;
+    exports it.polimi.ingsw.network.rmi;
+    exports it.polimi.ingsw.listener;
     opens it.polimi.ingsw.view.GUI.controllers to javafx.fxml, javafx. graphics;
     opens it.polimi.ingsw.model to com.google.gson;
+    opens it.polimi.ingsw.model.cards to com.google.gson;
 
 }
