57a2cf103ad6f410489705c957179caba23a7ac4
masiirene <irenepia.masi@mail.polimi.it>
2024-06-05 11:38:08 +0200

    JavaDoc for Chat and GameController

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Chat/Message.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Chat/Message.java
@@ -5,18 +5,31 @@ import it.polimi.ingsw.model.player.Player;
 import java.io.Serializable;
 import java.time.LocalTime;
 
+/**
+ * The Message class represents a message sent by a player in the chat.
+ */
 public class Message implements Serializable {
 
     private String text;
     private Player sender;
     private LocalTime time;
 
-    public Message(String text, Player sender){
+    /**
+     * Constructor that initializes a message with the given text and sender.
+     * The time of the message is set to the current time.
+     *
+     * @param text   the text content of the message
+     * @param sender the player who sends the message
+     */
+    public Message(String text, Player sender) {
         this.text = text;
         this.sender = sender;
         this.time = LocalTime.now();
     }
 
+    /**
+     * Default constructor that initializes a message with null values.
+     */
     public Message() {
         this.text = null;
         this.sender = null;
@@ -32,12 +45,13 @@ public class Message implements Serializable {
     }
 
     /**
-     * Set the text in the message to the param
+     * Set the text content of the message.
      * @param text text to set
      */
     public void setText(String text) {
         this.text = text;
     }
+
     /**
      *
      * @return the message's sender
@@ -47,15 +61,15 @@ public class Message implements Serializable {
     }
 
     /**
-     * Sets the message's sender
-     * @param sender sender
+     * Sets the message's sender.
+     * @param sender the sender to set
      */
     public void setSender(Player sender) {
         this.sender = sender;
     }
 
     /**
-     *
+     * Returns the time the message was sent.
      * @return the message's time of sending
      */
     public LocalTime getTime() {
@@ -63,8 +77,8 @@ public class Message implements Serializable {
     }
 
     /**
-     * Sets the message time to the parameter
-     * @param time param
+     * Sets the message time to the parameter.
+     * @param time the time to set
      */
     public void setTime(LocalTime time) {
         this.time = time;
@@ -72,18 +86,19 @@ public class Message implements Serializable {
 
     /**
      *
-     * @return the receiver
+     * @return the receiver of the message
      */
     public String whoIsReceiver() {
         return "*";
     }
 
     /**
-     * Returns the message in string format
-     * @param i
-     * @param len
-     * @param isPrivate
-     * @return
+     * Returns the message in string format.
+     *
+     * @param i the index of the message in chat
+     * @param len the maximum length of the message text
+     * @param isPrivate wheter the message is private
+     * @return the message
      */
     public String toString(int i, int len, boolean isPrivate) {
         String padding = " ".repeat(Math.max(0, (len - text.length())));
